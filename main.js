73e30c167cee60e6af86afebd1d8b008b57409b0
ResidentAleks123 <Aleksandartesinic720@gmail.com>
2026-06-27 23:15:53 +0200

    added StartButton for DFS

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startDFS());
 
 let boxesArray = [];
 
@@ -10,6 +11,9 @@ let columnAmount = 10;
 
 let boxWidth = 30;
 let boxHeight = 30;
+
+let startingPoint = 0;
+let endPoint = 0;
 
 canvas.width = boxWidth * columnAmount;
 canvas.height = boxHeight * rowAmount;
@@ -37,4 +41,25 @@ for(let i = 0; i < boxesArray.length; i++){
     ctx.rect(boxesArray[i].xPos, boxesArray[i].yPos, boxWidth, boxHeight);
     
     ctx.stroke();
+}
+
+drawStartingPoint();
+function drawStartingPoint(){
+    startingPoint = boxesArray[Math.floor(Math.random() * boxesArray.length)];
+    ctx.fillStyle = "black";
+    ctx.fillRect(startingPoint.xPos, startingPoint.yPos, boxWidth, boxHeight);
+}
+
+drawEndPoint();
+function drawEndPoint(){
+    endPoint = boxesArray[Math.floor(Math.random() * boxesArray.length)];
+    ctx.fillStyle = "red";
+    while(startingPoint == endPoint){
+        endPoint = boxesArray[Math.floor(Math.random() * boxesArray.length)];
+    }
+    ctx.fillRect(endPoint.xPos, endPoint.yPos, boxWidth, boxHeight);
+}
+
+function startDFS(){
+    
 }
